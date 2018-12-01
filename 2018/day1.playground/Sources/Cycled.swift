2f935d9251393bed7214e023b77b32dad195fc7c
Mark Sands <marksands07@gmail.com>
2018-12-01 10:55:42 -0600

    Advent Of Code 2018 - day1

---
+++ 2018/day1.playground/Sources/Cycled.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public func cycled<T>(_ input: [T]) -> UnfoldSequence<T, Int> {
+    return sequence(state: 0, next: { currentIndex -> T? in
+        let computedIndex = currentIndex % input.count
+        currentIndex += 1
+        return input[computedIndex]
+    })
+}
