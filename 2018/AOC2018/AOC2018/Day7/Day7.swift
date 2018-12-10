671417649f208537c03e30e5766a600ffe26eb96
Mark Sands <marksands07@gmail.com>
2018-12-10 14:09:34 -0600

    Day 7

--- 2018/AOC2018/AOC2018/Day7/Day7.swift
+++ 2018/AOC2018/AOC2018/Day7/Day7.swift
@@ -1,11 +1,66 @@
 import Foundation
 
 public final class Day7: Day {
+    private var dependencies: [String: [String]] = [:]
+    private var stepRank: [String: Int] = [:]
+    private var priorityQueue: [String] = []
+    private var workers: [String: Int] = [:]
+    private var totalTime = 0
+
+    public override init() {
+        super.init()
+
+        let regex = Regex(pattern: "^Step (\\w) must be finished before step (\\w) can begin.$")
+        
+        Input().trimmedInputCharactersByNewlines()
+            .map { regex.matches(in: $0)! }
+            .forEach {
+                dependencies[$0[1], default: []].append($0[2])
+                stepRank[$0[2], default: 0] += 1
+                stepRank[$0[1]] = stepRank[$0[1]] ?? 0
+        }
+        
+        priorityQueue = stepRank.filter { $0.value == 0 }.map { $0.key }.sorted()
+    }
+
     public override func part1() -> String {
-        return super.part1()
+        return topologicalSort(workerCount: 1)
     }
     
     public override func part2() -> String {
-        return super.part2()
+        _ = topologicalSort(workerCount: 5)
+        return "\(totalTime)"
+    }
+    
+    private func topologicalSort(workerCount: Int) -> String {
+        var result = ""
+
+        while priorityQueue.count > 0 || workers.keys.count > 0 {
+            let (node, t) = workers.min(by: { $0.value < $1.value }) ?? ("", 0)
+            workers.removeValue(forKey: node)
+            
+            totalTime = t
+            result += node
+            
+            for child in dependencies[node] ?? [] {
+                stepRank[child]? -= 1
+                
+                if stepRank[child] == 0 {
+                    priorityQueue.append(child)
+                    priorityQueue.sort()
+                }
+            }
+            
+            updateWorkers(maxWorkerCount: workerCount)
+        }
+        
+        return result
+    }
+    
+    private func updateWorkers(maxWorkerCount: Int) {
+        while workers.keys.count < maxWorkerCount && priorityQueue.count > 0 {
+            let node = priorityQueue.removeFirst()
+            workers[node] = totalTime + 60 + uppercaseLetters.exploded().firstIndex(of: node)! + 1
+        }
     }
 }
--- 2018/AOC2018/AOC2018/Day7/Day7Tests.swift
+++ 2018/AOC2018/AOC2018/Day7/Day7Tests.swift
@@ -5,10 +5,10 @@ class AOC2018_Day7_Tests: XCTestCase {
     let day = Day7()
     
     func testPart1() {
-        XCTAssertEqual("TBD", day.part1())
+        XCTAssertEqual("JKNSTHCBGRVDXWAYFOQLMPZIUE", day.part1())
     }
     
     func testPart2() {
-        XCTAssertEqual("TBD", day.part2())
+        XCTAssertEqual("755", day.part2())
     }
 }
