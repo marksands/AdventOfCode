f361c86fd245a0a3d8e8272e19562618639872a8
Mark Sands <marksands07@gmail.com>
2018-12-11 21:37:36 -0600

    Adds circular list

---
+++ 2018/AOC2018/AOC2018/Shared/CircularList.swift
@@ -0,0 +1,63 @@
+public class CircularList<T> {
+    public let value: T
+    
+    private var left: CircularList<T>!
+    private var right: CircularList<T>!
+    
+    public init(value: T) {
+        self.value = value
+        left = self
+        right = self
+    }
+    
+    @discardableResult
+    public func insertAfter(_ value: T) -> CircularList<T> {
+        let newNode = CircularList(value: value)
+        newNode.right = right
+        newNode.left = self
+
+        right.left = newNode
+        right = newNode
+        
+        return newNode
+    }
+    
+    @discardableResult
+    public func insertBefore(_ value: T) -> CircularList<T> {
+        let newNode = CircularList(value: value)
+        newNode.right = self
+        newNode.left = left
+        
+        left.right = newNode
+        left = newNode
+        
+        return newNode
+    }
+    
+    @discardableResult
+    public func remove() -> (left: CircularList<T>, right: CircularList<T>) {
+        defer { left = nil; right = nil}
+
+        let node = self
+        node.left.right = node.right
+        node.right.left = node.left
+        
+        return (left, right)
+    }
+    
+    public func advance(_ amount: Int) -> CircularList<T> {
+        var node = self
+        (0..<amount).forEach { _ in
+            node = node.right
+        }
+        return node
+    }
+    
+    public func reverse(_ amount: Int) -> CircularList<T> {
+        var node = self
+        (0..<amount).forEach { _ in
+            node = node.left
+        }
+        return node
+    }
+}
---
+++ 2018/AOC2018/AOC2018/Shared/CircularListTests.swift
@@ -0,0 +1,115 @@
+import XCTest
+import AOC2018
+
+class CircularListTests: XCTestCase {
+    func testCanCreateNewList() {
+        let testObject = CircularList(value: 0)
+        XCTAssertEqual(0, testObject.value)
+    }
+    
+    func testCircularListAddsValue() {
+        let testObject = CircularList(value: 0)
+        let nextObject = testObject.insertAfter(1)
+        
+        XCTAssertEqual(0, testObject.value)
+        XCTAssertEqual(1, nextObject.value)
+        
+        let valueTwo = testObject.insertAfter(2)
+        let valueThree = valueTwo.insertAfter(3)
+        
+        XCTAssertEqual(0, testObject.value)
+        XCTAssertEqual(1, nextObject.value)
+        XCTAssertEqual(2, valueTwo.value)
+        XCTAssertEqual(3, valueThree.value)
+    }
+    
+    func testCircularListInsertsAfterNode() {
+        let testObject = CircularList(value: 0).insertAfter(1).insertAfter(2).insertAfter(3)
+        XCTAssertEqual(3, testObject.value)
+    }
+    
+    func testCircularListAddsValueInMiddleOfList() {
+        let testObject = CircularList(value: 0)
+        let valueOne = testObject.insertAfter(1)
+        let valueThree = valueOne.insertAfter(3)
+        
+        XCTAssertEqual(0, testObject.value)
+        XCTAssertEqual(1, valueOne.value)
+        XCTAssertEqual(3, valueThree.value)
+
+        let valueTwo = valueOne.insertAfter(2)
+        
+        
+        XCTAssertEqual(0, testObject.value)
+        XCTAssertEqual(1, valueOne.value)
+        XCTAssertEqual(2, valueTwo.value)
+        XCTAssertEqual(3, valueThree.value)
+    }
+
+    func testCircularListRemovesValue() {
+        let testObject = CircularList(value: 0)
+        let valueOne = testObject.insertAfter(1)
+        valueOne.insertAfter(2)
+        
+        let (expectZero, expectTwo) = valueOne.remove()
+
+        XCTAssertEqual(0, expectZero.value)
+        XCTAssertEqual(2, expectTwo.value)
+        
+        let (expectZeroAgain, expectZeroCirclularly) = expectTwo.remove()
+
+        XCTAssertEqual(0, expectZeroAgain.value)
+        XCTAssertEqual(0, expectZeroCirclularly.value)
+    }
+    
+    func testCircularListRemoveReturnsLeftAndRightNode() {
+        let testObject = CircularList(value: 0)
+        let toRemove = testObject.insertAfter(1)
+        toRemove.insertAfter(2)
+
+        let (left, right) = toRemove.remove()
+        
+        XCTAssertEqual(0, left.value)
+        XCTAssertEqual(2, right.value)
+    }
+
+    func testCircularListInsertsBeforeNode() {
+        let testObject = CircularList(value: 0)
+        let valueOne = testObject.insertBefore(3).insertBefore(2).insertBefore(1)
+        
+        XCTAssertEqual(0, testObject.value)
+        XCTAssertEqual(1, valueOne.value)
+    }
+    
+    func testCircularListAdvances() {
+        var testObject = CircularList(value: 0)
+        testObject.insertAfter(1).insertAfter(2).insertAfter(3).insertAfter(4)
+
+        XCTAssertEqual(0, testObject.value)
+        
+        testObject = testObject.advance(1)
+        XCTAssertEqual(1, testObject.value)
+        
+        testObject = testObject.advance(2)
+        XCTAssertEqual(3, testObject.value)
+
+        testObject = testObject.advance(2)
+        XCTAssertEqual(0, testObject.value)
+    }
+    
+    func testCircularListReverses() {
+        var testObject = CircularList(value: 0)
+        testObject.insertAfter(1).insertAfter(2).insertAfter(3).insertAfter(4)
+        
+        XCTAssertEqual(0, testObject.value)
+        
+        testObject = testObject.reverse(1)
+        XCTAssertEqual(4, testObject.value)
+        
+        testObject = testObject.reverse(2)
+        XCTAssertEqual(2, testObject.value)
+        
+        testObject = testObject.reverse(2)
+        XCTAssertEqual(0, testObject.value)
+    }
+}
