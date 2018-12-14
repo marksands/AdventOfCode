f5c281fb60035e7a4a27dadfb7bfe554919423f4
Mark Sands <marksands07@gmail.com>
2018-12-14 11:14:26 -0600

    Day 14

--- 2018/AOC2018/AOC2018/Day14/Day14.swift
+++ 2018/AOC2018/AOC2018/Day14/Day14.swift
@@ -1,11 +1,67 @@
 import Foundation
 
 public final class Day14: Day {
+    private let input = 765071
+
     public override func part1() -> String {
-        return super.part1()
+        let list = CircularList(value: 3)
+        let root = list
+        
+        list.insertAfter(7)
+        
+        var recipe1Node = root
+        var recipe2Node = root.advance(by: 1)
+        
+        var step = 0
+        while step < (input + 10) {
+            let sumOfRecipe = recipe1Node.value + recipe2Node.value
+            
+            if sumOfRecipe >= 10 {
+                root.insertBefore(1)
+                step += 1
+            }
+
+            root.insertBefore(sumOfRecipe % 10)
+            step += 1
+            
+            recipe1Node = recipe1Node.advance(by: recipe1Node.value + 1)
+            recipe2Node = recipe2Node.advance(by: recipe2Node.value + 1)
+        }
+        
+        return (input..<(input + 10)).reduce([]) { $0 + [root.advance(by: $1).value] }.map { String($0) }.joined()
     }
     
     public override func part2() -> String {
-        return super.part2()
+        var values: [Int] = [3, 7]
+        let expectedResult = String(input).exploded().compactMap(Int.init)
+
+        var recipe1Index = 0
+        var recipe2Index = 1
+        
+        var step = 2
+        while true {
+            let recipe1Score = values[recipe1Index]
+            let recipe2Score = values[recipe2Index]
+            let sumOfRecipe = recipe1Score + recipe2Score
+            
+            if sumOfRecipe >= 10 {
+                values.append(1)
+                step += 1
+                
+                if values.count > expectedResult.count, Array(values.suffix(from: values.count - expectedResult.count)) == expectedResult {
+                    return "\(step - expectedResult.count)"
+                }
+            }
+            
+            values.append(sumOfRecipe % 10)
+            step += 1
+            
+            if values.count > expectedResult.count, Array(values.suffix(from: values.count - expectedResult.count)) == expectedResult {
+                return "\(step - expectedResult.count)"
+            }
+            
+            recipe1Index = (recipe1Index + recipe1Score + 1) % values.count
+            recipe2Index = (recipe2Index + recipe2Score + 1) % values.count
+        }
     }
 }
--- 2018/AOC2018/AOC2018/Day14/Day14Tests.swift
+++ 2018/AOC2018/AOC2018/Day14/Day14Tests.swift
@@ -2,13 +2,13 @@ import XCTest
 import AOC2018
 
 class AOC2018_Day14_Tests: XCTestCase {
-    let day = Day()
+    let day = Day14()
     
     func testPart1() {
-        XCTAssertEqual("TBD", day.part1())
+        XCTAssertEqual("3171123923", day.part1())
     }
-    
+
     func testPart2() {
-        XCTAssertEqual("TBD", day.part2())
+        XCTAssertEqual("20353748", day.part2())
     }
 }
