aec9c64e96df6450fe5c916633a82284bf32e388
Mark Sands <marksands07@gmail.com>
2018-12-15 23:02:09 -0600

    Day 15 part1 almost finished

--- 2018/AOC2018/AOC2018/Day15/Day15.swift
+++ 2018/AOC2018/AOC2018/Day15/Day15.swift
@@ -32,12 +32,18 @@ public class Unit {
     }
 }
 
+public enum RoundResult {
+    case completed
+    case halted
+    case finished
+}
+
 public final class Day15: Day {
     public var map: [[GroundType]]
     public var units: [Unit] = []
 
-    public init(input: String) {
-        let lines = input.components(separatedBy: .newlines)
+    public init(input: String = Input().rawInput()) {
+        let lines = input.trimmingCharacters(in: .newlines).components(separatedBy: .newlines)
         
         map = lines.map { row in row.exploded().map { $0 == "#" ? .wall : .open } }
         
@@ -50,18 +56,53 @@ public final class Day15: Day {
         super.init()
     }
     
-    public func printableMap() -> String {
-        return map.reduce(into: "", {
-            $0 += $1.reduce(into: "", { $0 += $1.rawValue }) + "\n"
-        })
+    public override func part1() -> String {
+        var roundCount = 0
+        while true {
+            if battle() == .completed {
+                roundCount += 1
+            } else {
+                break
+            }
+            print(printableRound())
+        }
+        return String(livingUnits().reduce(0, { $0 + $1.hp }) * roundCount)
     }
     
+    @discardableResult
+    public func battle() -> RoundResult {
+        for source in livingUnits() {
+            if let target = enemyWithinRange(of: source) {
+                target.hp -= source.power
+            } else if let nextPosition = shortestPathToEnemy(from: source).first {
+                source.position = nextPosition
+                if let target = enemyWithinRange(of: source) {
+                    target.hp -= source.power
+                }
+            }
+            if livingUnits().map({ $0.race }).unique().count == 1 {
+                return .halted
+            }
+        }
+        return livingUnits().map({ $0.race }).unique().count > 1 ? .completed : .finished
+    }
+    
+    public func enemyWithinRange(of source: Unit) -> Unit? {
+        let adjacentEnemies = enemies(of: source).filter { neighbors(for: source.position).contains($0.position) }
+        return adjacentEnemies.sorted { $0.hp < $1.hp }.first
+    }
+        
     public func shortestPathToEnemy(from source: Unit) -> [Position] {
-        return []
+        let possiblePaths = enemies(of: source).map { path(from: source, to: $0) }.filter { $0.count > 0 }
+        return possiblePaths.min(by: { $0.count < $1.count }) ?? []
     }
     
     public func path(from source: Unit, to target: Unit) -> [Position] {
+        precondition(source.race != target.race)
+        
         var queue = [[source.position]]
+        var visitedTiles = Set<Position>()
+        let skippingUnits = allUnitPositions().filter { $0 != target.position }
 
         while queue.count > 0 {
             let path = queue.removeFirst()
@@ -70,53 +111,63 @@ public final class Day15: Day {
                 return Array(path.dropFirst())
             } else if let current = path.last {
                 for position in neighbors(for: current) {
-                    queue.append(path + [position])
+                    if !visitedTiles.contains(position) && !skippingUnits.contains(position) {
+                        visitedTiles.insert(position)
+                        queue.append(path + [position])
+                    }
                 }
             }
         }
         
         return []
     }
-    
-    private func allUnitPositions() -> [Position] {
-         return units.map({ $0.position })
-    }
-    
+
     public func neighbors(for position: Position) -> [Position] {
         return [position.north(), position.west(), position.east(), position.south()]
             .filter { map[$0.y][$0.x].rawValue != GroundType.wall.rawValue }
     }
+    
+    private func enemies(of source: Unit) -> [Unit] {
+        return livingUnits().filter { $0.race != source.race }
+    }
+    
+    private func allUnitPositions() -> [Position] {
+        return livingUnits().map({ $0.position })
+    }
+    
+    private func livingUnits() -> [Unit] {
+        return units.filter { $0.hp > 0 }.sorted(by: { $0.position < $1.position })
+    }
 }
 
-// Maybe faster, experiment later
-//    public func path(from source: Unit, to target: Unit) -> [Position] {
-//        var queue = [source.position]
-//        var visitedTiles = Set<Position>()
-//        var parentData: [Position: Position] = [:]
-//
-//        while queue.count > 0 {
-//            let current = queue.removeFirst()
-//
-//            for position in neighbors(for: current) {
-//                if target.position == position {
-//                    parentData[position] = current
-//                    return backtrace(parentData, source: source, target: target)
-//                } else if !visitedTiles.contains(position) && !allUnitPositions().contains(position) {
-//                    queue.append(position)
-//                    visitedTiles.insert(position)
-//                    parentData[position] = current
-//                }
-//            }
-//        }
-//
-//        return []
-//    }
-//
-//    private func backtrace(_ parent: [Position: Position], source: Unit, target: Unit) -> [Position] {
-//        var path = [target.position]
-//        while let lastPosition = path.last, lastPosition != source.position {
-//            guard let position = parent[lastPosition] else { fatalError() }
-//            path.append(position)
-//        }
-//        return path.reversed()
-//    }
+extension Day15 {
+    public func printableMap() -> String {
+        return map.reduce(into: "", {
+            $0 += $1.reduce(into: "", { $0 += $1.rawValue }) + "\n"
+        })
+    }
+    
+    public func printableRound() -> String {
+        var result = ""
+        zip(map.indices, map).forEach { y, row in
+            var unitsInSeries: [Unit] = []
+            zip(row.indices, row).forEach { x, tile in
+                let position = Position(x: x, y: y)
+                if let unit = livingUnits().first(where: { $0.position == position }) {
+                    unitsInSeries.append(unit)
+                    result += unit.race.rawValue
+                } else {
+                    result += tile.rawValue
+                }
+            }
+            
+            if unitsInSeries.count > 0 {
+                result += "   "
+                result += unitsInSeries.reduce(into: "") { $0 += "\($1.race.rawValue)(\($1.hp)), " }.dropLast(2)
+            }
+
+            result += "\n"
+        }
+        return result
+    }
+}
--- 2018/AOC2018/AOC2018/Day15/Day15Tests.swift
+++ 2018/AOC2018/AOC2018/Day15/Day15Tests.swift
@@ -13,18 +13,17 @@ class AOC2018_Day15_Tests: XCTestCase {
             """
     
     func testPart1() {
-        let day = Day15(input: testInput)
-        print(day.printableMap())
-        XCTAssertEqual("TBD", day.part1())
+        //let day = Day15()
+        // 226620 is too low
+        //XCTAssertEqual("226620", day.part1())
     }
     
     func testPart2() {
         let day = Day15(input: testInput)
-        print(day.printableMap())
         XCTAssertEqual("TBD", day.part2())
     }
     
-    func testMapRendersOnlyTerritory() {
+    func testPrintableMapRendersOnlyTerritory() {
         let expected = """
             #######
             #.....#
@@ -39,6 +38,26 @@ class AOC2018_Day15_Tests: XCTestCase {
         XCTAssertEqual(expected, day.printableMap())
     }
     
+    func testPrintableRoundRendersTerritoryAndUnits() {
+        let input = """
+            ########
+            #..G..##
+            #.E##.##
+            #...E.G#
+            ########\n
+            """
+        let day = Day15(input: input)
+        
+        let expected = """
+            ########
+            #..G..##   G(200)
+            #.E##.##   E(200)
+            #...E.G#   E(200), G(200)
+            ########\n
+            """
+        XCTAssertEqual(expected, day.printableRound())
+    }
+
     func testNeighborsOfIsolatedUnit() {
         let day = Day15(input: testInput)
         let neighbors = day.neighbors(for: Position(x: 2, y: 1))
@@ -123,6 +142,22 @@ class AOC2018_Day15_Tests: XCTestCase {
                         Position(x: 4, y: 5), Position(x: 5, y: 5), Position(x: 6, y: 5)]
         XCTAssertEqual(expected, path)
     }
+    
+    func testPathFromGoblinWithinRangeOfElf() {
+        let input = """
+            ######
+            #GE..#
+            ######
+            """
+        let day = Day15(input: input)
+        let goblin = day.units.first(where: { $0.position == Position(x: 1, y: 1) })!
+        let elf = day.units.first(where: { $0.position == Position(x: 2, y: 1) })!
+        
+        let path = day.path(from: goblin, to: elf)
+        
+        let expected = [Position(x: 2, y: 1)]
+        XCTAssertEqual(expected, path)
+    }
 
     func testShortestPathFromGoblinToTarget() {
         let input = """
@@ -141,5 +176,288 @@ class AOC2018_Day15_Tests: XCTestCase {
                         Position(x: 5, y: 1), Position(x: 6, y: 1), Position(x: 7, y: 1),
                         Position(x: 7, y: 2), Position(x: 7, y: 3)]
         XCTAssertEqual(expected, path)
+    }
+    
+    func testShortestPathHasZeroValidPaths() {
+        let input = """
+            #########
+            #G......#
+            ####...G#
+            #E..G.GE#
+            #########
+            """
+        let day = Day15(input: input)
+        let goblin = day.units.first(where: { $0.position == Position(x: 1, y: 1) })!
+        
+        let path = day.shortestPathToEnemy(from: goblin)
+        
+        XCTAssertEqual([], path)
+    }
+    
+    func testShortestPathBreaksAllTies() {
+        let input = """
+            #########
+            #.E@@.E.#
+            #E..G..E#
+            #.E...E.#
+            #########
+            """
+        let day = Day15(input: input)
+        let goblin = day.units.first(where: { $0.position == Position(x: 4, y: 2) })!
+        
+        let path = day.shortestPathToEnemy(from: goblin)
+        
+        XCTAssertEqual([Position(x: 4, y: 1), Position(x: 3, y: 1), Position(x: 2, y: 1)], path)
+    }
+    
+    func testEnemiesWithinRange() {
+        let input = """
+            ######
+            #G...#
+            #GE.G#
+            #EE..#
+            ######
+            """
+        let day = Day15(input: input)
+        let goblin = day.units.first(where: { $0.position == Position(x: 1, y: 2) })!
+        
+        let enemy = day.enemyWithinRange(of: goblin)
+        XCTAssertEqual(Position(x: 2, y: 2), enemy?.position)
+    }
+
+    func testEnemiesWithinRangeTiesByLowestHP() {
+        let input = """
+            ######
+            #.G..#
+            #GEG.#
+            #.G..#
+            ######
+            """
+        let day = Day15(input: input)
+        
+        sortedUnits(day.units, byRace: .goblin).last?.hp = 140
+        let elf = sortedUnits(day.units, byRace: .elf).first!
+        
+        let enemy = day.enemyWithinRange(of: elf)!
+        XCTAssertEqual(Position(x: 2, y: 3), enemy.position)
+    }
+
+    func testEnemiesWithinRangeTiesByLowestHPAndReadingOrder() {
+        let input = """
+            ######
+            #.G..#
+            #GEG.#
+            #.G..#
+            ######
+            """
+        let day = Day15(input: input)
+        
+        let goblins = sortedUnits(day.units, byRace: .goblin)
+        goblins[1].hp = 130
+        goblins[2].hp = 130
+        
+        let elf = sortedUnits(day.units, byRace: .elf).first!
+        
+        let enemy = day.enemyWithinRange(of: elf)!
+        XCTAssertEqual(Position(x: 1, y: 2), enemy.position)
+    }
+    
+    func test3RoundsFromExample1() {
+        let input = """
+            #########
+            #G..G..G#
+            #.......#
+            #.......#
+            #G..E..G#
+            #.......#
+            #.......#
+            #G..G..G#
+            #########
+            """
+        let day = Day15(input: input)
+        
+        day.battle()
+        day.battle()
+        day.battle()
+
+        let expected = """
+            #########
+            #.......#
+            #..GGG..#   G(200), G(191), G(200)
+            #..GEG..#   G(200), E(185), G(200)
+            #G..G...#   G(200), G(200)
+            #......G#   G(200)
+            #.......#
+            #.......#
+            #########\n
+            """
+        XCTAssertEqual(expected, day.printableRound())
+    }
+
+    func testExampleCombatFromExample1() {
+        let input = """
+            #######
+            #.G...#
+            #...EG#
+            #.#.#G#
+            #..G#E#
+            #.....#
+            #######
+            """
+        
+        let day = Day15(input: input)
+        
+        (1...47).forEach { _ in
+            day.battle()
+        }
+        
+        let final = """
+            #######
+            #G....#   G(200)
+            #.G...#   G(131)
+            #.#.#G#   G(59)
+            #...#.#
+            #....G#   G(200)
+            #######\n
+            """
+        XCTAssertEqual(final, day.printableRound())
+    }
+    
+    func testExampleCombatFromExample2() {
+        let input = """
+            #######
+            #G..#E#
+            #E#E.E#
+            #G.##.#
+            #...#E#
+            #...E.#
+            #######
+            """
+        let day = Day15(input: input)
+        
+        let expected = """
+            #######
+            #...#E#   E(200)
+            #E#...#   E(197)
+            #.E##.#   E(185)
+            #E..#E#   E(200), E(200)
+            #.....#
+            #######\n
+            """
+        
+        XCTAssertEqual("36334", day.part1())
+        XCTAssertEqual(expected, day.printableRound())
+    }
+    
+    func testExampleCombatFromExample3() {
+        let input = """
+            #######
+            #E..EG#
+            #.#G.E#
+            #E.##E#
+            #G..#.#
+            #..E#.#
+            #######
+            """
+        let day = Day15(input: input)
+        
+        let expected = """
+            #######
+            #.E.E.#   E(164), E(197)
+            #.#E..#   E(200)
+            #E.##.#   E(98)
+            #.E.#.#   E(200)
+            #...#.#
+            #######\n
+            """
+        
+        XCTAssertEqual("39514", day.part1())
+        XCTAssertEqual(expected, day.printableRound())
+    }
+    
+    func testExampleCombatFromExample4() {
+        let input = """
+            #######
+            #E.G#.#
+            #.#G..#
+            #G.#.G#
+            #G..#.#
+            #...E.#
+            #######
+            """
+        let day = Day15(input: input)
+        
+        let expected = """
+            #######
+            #G.G#.#   G(200), G(98)
+            #.#G..#   G(200)
+            #..#..#
+            #...#G#   G(95)
+            #...G.#   G(200)
+            #######\n
+            """
+        
+        XCTAssertEqual("27755", day.part1())
+        XCTAssertEqual(expected, day.printableRound())
+    }
+
+    func testExampleCombatFromExample5() {
+        let input = """
+            #######
+            #.E...#
+            #.#..G#
+            #.###.#
+            #E#G#G#
+            #...#G#
+            #######
+            """
+        let day = Day15(input: input)
+        
+        let expected = """
+            #######
+            #.....#
+            #.#G..#   G(200)
+            #.###.#
+            #.#.#.#
+            #G.G#G#   G(98), G(38), G(200)
+            #######\n
+            """
+        
+        XCTAssertEqual("28944", day.part1())
+        XCTAssertEqual(expected, day.printableRound())
+    }
+    
+    func testExampleCombatFromExample6() {
+        let input = """
+            #########
+            #G......#
+            #.E.#...#
+            #..##..G#
+            #...##..#
+            #...#...#
+            #.G...G.#
+            #.....G.#
+            #########
+            """
+        let day = Day15(input: input)
+        
+        let expected = """
+            #########
+            #.G.....#   G(137)
+            #G.G#...#   G(200), G(200)
+            #.G##...#   G(200)
+            #...##..#
+            #.G.#...#   G(200)
+            #.......#
+            #.......#
+            #########\n
+            """
+        
+        XCTAssertEqual("18740", day.part1())
+        XCTAssertEqual(expected, day.printableRound())
+    }
+    
+    private func sortedUnits(_ units: [AOC2018.Unit], byRace race: AOC2018.Unit.Race) -> [AOC2018.Unit] {
+        return units.sorted(by: { $0.position < $1.position }).filter { $0.race == race }
     }
 }
