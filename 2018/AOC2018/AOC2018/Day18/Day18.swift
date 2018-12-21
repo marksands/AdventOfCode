04dc0714ece79a9c1752660bfc05b08f1006b20e
Mark Sands <marksands07@gmail.com>
2018-12-20 23:03:08 -0600

    Day 18 optimized

--- 2018/AOC2018/AOC2018/Day18/Day18.swift
+++ 2018/AOC2018/AOC2018/Day18/Day18.swift
@@ -6,97 +6,83 @@ public enum WoodlandType: String {
     case open = "."
 }
 
-public final class Acre: Equatable, Hashable {
-    public static func == (lhs: Acre, rhs: Acre) -> Bool {
-        return lhs.position == rhs.position
-    }
-    
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(position)
-    }
-    
-    public var woodlandType: WoodlandType
-    public let position: Position
-    
-    public init(position: Position, type: WoodlandType) {
-        self.position = position
-        self.woodlandType = type
-    }
-}
-
 public final class Day18: Day {
-    private let woodland: [Acre]
+    private var woodland: [Position: WoodlandType] = [:]
+    private var treeCount = 0
+    private var lumberyardCount = 0
     
-    public init(input: String = Input().trimmedRawInput()) {
-        woodland = input.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: .newlines).enumerated().flatMap { y, line in
-            line.exploded().enumerated().map { x, tile -> Acre in
+    public override init() {
+        super.init()
+        
+        Input().trimmedInputCharactersByNewlines().enumerated().forEach { y, line in
+            line.exploded().enumerated().forEach { x, tile in
                 let position = Position(x: x, y: y)
-                return Acre(position: position, type: WoodlandType(rawValue: tile)!)
+                let type = WoodlandType(rawValue: tile)!
+                if type == .lumberyard {
+                    lumberyardCount += 1
+                } else if type == .trees {
+                    treeCount += 1
+                }
+                woodland[position] = type
             }
         }
-
-        super.init()
     }
     
     public override func part1() -> String {
-        (1...10).forEach { round in
-            mutate()
-        }
-        let lumberyards = woodland.count(where: { $0.woodlandType == .lumberyard })
-        let trees = woodland.count(where: { $0.woodlandType == .trees })
-        return String(lumberyards * trees)
+        (1...10).forEach { _ in mutate() }
+        return String(lumberyardCount * treeCount)
     }
     
     public override func part2() -> String {
-        (1...1_000_000_000).forEach { round in
+        var previousResults: [Int: Int] = [:]
+        var result = 0
+        var previous = 0
+        
+        for round in (1...) {
             mutate()
-            let lumberyards = woodland.count(where: { $0.woodlandType == .lumberyard })
-            let trees = woodland.count(where: { $0.woodlandType == .trees })
-            print("\(lumberyards), \(trees), \(lumberyards*trees)")
+            result = lumberyardCount*treeCount
+            let current = round - previousResults[result, default: 0]
+            previousResults[result] = round
+
+            if current == previous && 1_000_000_000 % current == round % current {
+                return String(result)
+            }
+            previous = current
         }
-        let lumberyards = woodland.count(where: { $0.woodlandType == .lumberyard })
-        let trees = woodland.count(where: { $0.woodlandType == .trees })
-        return String(lumberyards * trees)
+        return String(result)
     }
 
     public func mutate() {
         var positionChanges: [Position: WoodlandType] = [:]
         
-        woodland.forEach { acre in
-            let surroundedPositions = Set(acre.position.surrounding())
-
-            if acre.woodlandType == .open {
-                let nearbyTrees = woodland.count(where: { surroundedPositions.contains($0.position) && $0.woodlandType == .trees })
+        (0..<2500).forEach { index in
+            let position = Position(x: index / 50, y: index % 50)
+            let woodlandType = woodland[position]
+            let surroundedTiles = Set(position.surrounding()).compactMap { woodland[$0] }
+            
+            if woodlandType == .open {
+                let nearbyTrees = surroundedTiles.count(where: { $0 == .trees })
                 if nearbyTrees >= 3 {
-                    positionChanges.updateValue(.trees, forKey: acre.position)
+                    treeCount += 1
+                    positionChanges.updateValue(.trees, forKey: position)
                 }
-            } else if acre.woodlandType == .trees {
-                let nearbyLumberYards = woodland.count(where: { surroundedPositions.contains($0.position) && $0.woodlandType == .lumberyard })
+            } else if woodlandType == .trees {
+                let nearbyLumberYards = surroundedTiles.count(where: { $0 == .lumberyard })
                 if nearbyLumberYards >= 3 {
-                    positionChanges.updateValue(.lumberyard, forKey: acre.position)
+                    treeCount -= 1
+                    lumberyardCount += 1
+                    positionChanges.updateValue(.lumberyard, forKey: position)
                 }
-            } else if acre.woodlandType == .lumberyard {
-                let nearbyTrees = woodland.count(where: { surroundedPositions.contains($0.position) && $0.woodlandType == .trees })
-                let nearbyLumberYards = woodland.count(where: { surroundedPositions.contains($0.position) && $0.woodlandType == .lumberyard })
-                if !(nearbyTrees > 0 && nearbyLumberYards > 0) {
-                    positionChanges.updateValue(.open, forKey: acre.position)
+            } else if woodlandType == .lumberyard {
+                if !(surroundedTiles.count(where: { $0 == .trees }) > 0 && surroundedTiles.count(where: { $0 == .lumberyard }) > 0) {
+                    lumberyardCount -= 1
+                    positionChanges.updateValue(.open, forKey: position)
                 }
             }
         }
         
         for (position, type) in positionChanges {
-            woodland[position.x + 50 * position.y].woodlandType = type
-        }
-    }
-    
-    public func boundingRect() -> CGRect {
-        let dimension = Int(sqrt(Double(woodland.count)))
-        return CGRect(x: 0, y: 0, width: dimension, height: dimension)
-    }
-    
-    public func printableBoard() -> String {
-        return woodland.chunks(ofSize: Int(boundingRect().width)).reduce(into: "") { $0 +=
-            $1.reduce(into: "") { $0 += $1.woodlandType.rawValue } + "\n"
+            woodland[position] = type
         }
     }
 }
