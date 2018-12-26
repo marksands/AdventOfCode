4e789a673acc7a51b09f0c089067141fe6921666
Mark Sands <marksands07@gmail.com>
2018-12-25 23:03:55 -0600

    Day 20 refactored

--- 2018/AOC2018/AOC2018/Day20/Day20.swift
+++ 2018/AOC2018/AOC2018/Day20/Day20.swift
@@ -1,11 +1,49 @@
 import Foundation
 
+enum MazeTile: String {
+    case start = "^"
+    case end = "$"
+    case N = "N"
+    case S = "S"
+    case E = "E"
+    case W = "W"
+    case branchOpen = "("
+    case branchClose = ")"
+    case pipe = "|"
+}
+
 public final class Day20: Day {
+    private var distances: [Position: Int] = [:]
+
+    public override init() {
+        super.init()
+        
+        var positions: [Position] = []
+        var currentPosition = Position(x: 0, y: 0)
+        
+        Input().trimmedRawInput().exploded().compactMap(MazeTile.init).forEach { tile in
+            switch tile {
+            case .start, .end:
+                break
+            case .branchOpen:
+                positions.append(currentPosition)
+            case .branchClose:
+                currentPosition = positions.popLast()!
+            case .pipe:
+                currentPosition = positions.last!
+            case .N, .S, .E, .W:
+                let newPosition = [MazeTile.N: currentPosition.north(), MazeTile.S: currentPosition.south(), MazeTile.E: currentPosition.east(), MazeTile.W: currentPosition.west()][tile]!
+                distances[newPosition] = min(distances[newPosition, default: Int.max], distances[currentPosition, default: 0] + 1)
+                currentPosition = newPosition
+            }
+        }
+    }
+
     public override func part1() -> String {
-        return super.part1()
+        return String(distances.values.max()!)
     }
     
     public override func part2() -> String {
-        return super.part2()
+        return String(distances.values.count(where: { $0 >= 1000 }))
     }
 }
--- 2018/AOC2018/AOC2018/Day20/Day20Tests.swift
+++ 2018/AOC2018/AOC2018/Day20/Day20Tests.swift
@@ -5,10 +5,10 @@ class AOC2018_Day20_Tests: XCTestCase {
     let day = Day20()
     
     func testPart1() {
-        XCTAssertEqual("TBD", day.part1())
+        XCTAssertEqual("3879", day.part1())
     }
     
     func testPart2() {
-        XCTAssertEqual("TBD", day.part2())
+        XCTAssertEqual("8464", day.part2())
     }
 }
