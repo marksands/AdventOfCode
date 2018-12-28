86d2fd4f8a7df0f9b568bac8019975075c21f816
Mark Sands <marksands07@gmail.com>
2018-12-28 14:46:25 -0600

    Day 23 refactor

--- 2018/AOC2018/AOC2018/Day23/Day23.swift
+++ 2018/AOC2018/AOC2018/Day23/Day23.swift
@@ -32,70 +32,30 @@ public final class Day23: Day {
         var xRange = nanobots.map { $0.position.x }.min()!...nanobots.map { $0.position.x }.max()!
         var yRange = nanobots.map { $0.position.y }.min()!...nanobots.map { $0.position.y }.max()!
         var zRange = nanobots.map { $0.position.z }.min()!...nanobots.map { $0.position.z }.max()!
-        var midX = (xRange.lowerBound + xRange.upperBound)/2
-        var midY = (yRange.lowerBound + yRange.upperBound)/2
-        var midZ = (zRange.lowerBound + zRange.upperBound)/2
         
         var coordinate = Position.zero
         var maxCount = 0
-
-        let validatePosition = { [unowned self] (position: Position) in
-            let count = self.nanobots.count(where: { $0.position.manhattenDistance(to: position) <= $0.radius })
-            if count > maxCount || (count == maxCount && position.manhattenDistance(to: .zero) < coordinate.manhattenDistance(to: .zero)) {
-                print("Got \(count) for \(position)")
-                maxCount = count
-                coordinate = position
-            }
-        }
         
-        (1...32).reversed().map { 1 << $0 }.forEach { step in
-            stride(from: xRange.lowerBound, to: midX, by: step).forEach { x in
-                stride(from: yRange.lowerBound, to: midY, by: step).forEach { y in
-                    stride(from: zRange.lowerBound, to: midZ, by: step).forEach { z in
-                        validatePosition(Position(x: x, y: y, z: z))
-                    }
-                    stride(from: midZ, to: zRange.upperBound, by: step).forEach { z in
-                        validatePosition(Position(x: x, y: y, z: z))
-                    }
-                }
-                stride(from: midY, to: yRange.upperBound, by: step).forEach { y in
-                    stride(from: zRange.lowerBound, to: midZ, by: step).forEach { z in
-                        validatePosition(Position(x: x, y: y, z: z))
-                    }
-                    stride(from: midZ, to: zRange.upperBound, by: step).forEach { z in
-                        validatePosition(Position(x: x, y: y, z: z))
-                    }
-                }
-            }
-
-            stride(from: midX, to: xRange.upperBound, by: step).forEach { x in
-                stride(from: yRange.lowerBound, to: midY, by: step).forEach { y in
-                    stride(from: zRange.lowerBound, to: midZ, by: step).forEach { z in
-                        validatePosition(Position(x: x, y: y, z: z))
-                    }
-                    stride(from: midZ, to: zRange.upperBound, by: step).forEach { z in
-                        validatePosition(Position(x: x, y: y, z: z))
-                    }
-                }
-                stride(from: midY, to: yRange.upperBound, by: step).forEach { y in
-                    stride(from: zRange.lowerBound, to: midZ, by: step).forEach { z in
-                        validatePosition(Position(x: x, y: y, z: z))
-                    }
-                    stride(from: midZ, to: zRange.upperBound, by: step).forEach { z in
-                        validatePosition(Position(x: x, y: y, z: z))
+        (0...32).reversed().map { 1 << $0 }.forEach { step in
+            stride(from: xRange.lowerBound, to: xRange.upperBound, by: step).forEach { x in
+                stride(from: yRange.lowerBound, to: yRange.upperBound, by: step).forEach { y in
+                    stride(from: zRange.lowerBound, to: zRange.upperBound, by: step).forEach { z in
+                        let position = Position(x: x, y: y, z: z)
+                        let count = self.nanobots.count(where: { $0.position.manhattenDistance(to: position) <= $0.radius })
+                        if count > maxCount || (count == maxCount && position.manhattenDistance(to: .zero) < coordinate.manhattenDistance(to: .zero)) {
+                            print("Got \(count) for \(position)")
+                            maxCount = count
+                            coordinate = position
+                        }
                     }
                 }
             }
             
-            let range = (step * 58)/2
+            let range = (step * 60)/2
             
             xRange = (coordinate.x - range)...(coordinate.x + range)
             yRange = (coordinate.y - range)...(coordinate.y + range)
             zRange = (coordinate.z - range)...(coordinate.z + range)
-
-            midX = (xRange.lowerBound + xRange.upperBound)/2
-            midY = (yRange.lowerBound + yRange.upperBound)/2
-            midZ = (zRange.lowerBound + zRange.upperBound)/2
         }
         
         return "\(coordinate.manhattenDistance(to: .zero))"
--- 2018/AOC2018/AOC2018/Shared/Position.swift
+++ 2018/AOC2018/AOC2018/Shared/Position.swift
@@ -1,10 +1,14 @@
 public struct Position: Equatable, Hashable {
     public var x: Int
     public var y: Int
+    public var z: Int
+    public var w: Int
     
-    public init(x: Int, y: Int) {
+    public init(x: Int, y: Int, z: Int = 0, w: Int = 0) {
         self.x = x
         self.y = y
+        self.z = z
+        self.w = w
     }
     
     public func north() -> Position {
@@ -27,6 +31,14 @@ public struct Position: Equatable, Hashable {
         return [north().west(), north(), north().east(),
                 west(), east(),
                 south().west(), south(), south().east()]
+    }
+    
+    public func manhattenDistance(to p2: Position) -> Int {
+        return abs(Int(w - p2.w)) + abs(Int(z - p2.z)) + abs(Int(y - p2.y)) + abs(Int(x - p2.x))
+    }
+    
+    public static var zero: Position {
+        return Position(x: 0, y: 0)
     }
 }
 
