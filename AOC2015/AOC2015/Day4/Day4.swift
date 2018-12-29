58a215d3bf6143154cd41e16c775d128c342c748
Mark Sands <marksands07@gmail.com>
2018-12-29 15:55:07 -0600

    Day 4 fast

--- AOC2015/AOC2015/Day4/Day4.swift
+++ AOC2015/AOC2015/Day4/Day4.swift
@@ -1,12 +1,29 @@
 import Foundation
 import AdventOfCode
+import CommonCrypto
 
 public final class Day4: Day {
+    private let seed = "ckczppom"
+
     public override func part1() -> String {
-        return "TBD"
+        return String(firstNumberMatchingHash(0xffff_f000_0000_0000))
     }
     
     public override func part2() -> String {
-        return "TBD"
+        return String(firstNumberMatchingHash(0xffff_ff00_0000_0000))
+    }
+    
+    private func firstNumberMatchingHash(_ match: UInt64) -> Int {
+        let result = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(CC_MD5_DIGEST_LENGTH))
+        defer { result.deallocate() }
+        
+        for number in (1...) {
+            let value = (seed + String(number))
+            CC_MD5(value, CC_LONG(value.count), result)
+            if result.withMemoryRebound(to: UInt64.self, capacity: 16, { $0.pointee.bigEndian & match == 0 }) {
+                return number
+            }
+        }
+        fatalError()
     }
 }
--- AOC2015/AOC2015/Day4/Day4Tests.swift
+++ AOC2015/AOC2015/Day4/Day4Tests.swift
@@ -5,10 +5,10 @@ class AOC2015_Day4_Tests: XCTestCase {
     let day = Day4()
     
     func testPart1() {
-        XCTAssertEqual("TBD", day.part1())
+        XCTAssertEqual("117946", day.part1())
     }
     
     func testPart2() {
-        XCTAssertEqual("TBD", day.part2())
+        XCTAssertEqual("3938038", day.part2())
     }
 }
