c3d59ba11d47645f952cd449ec02a688d228f2a2
Mark Sands <marksands07@gmail.com>
2018-12-29 09:50:01 -0600

    Day 3 initial

---
+++ AOC2015/AOC2015/Day3/Day3.swift
@@ -0,0 +1,12 @@
+import Foundation
+import AdventOfCode
+
+public final class Day3: Day {
+    public override func part1() -> String {
+        return "TBD"
+    }
+    
+    public override func part2() -> String {
+        return "TBD"
+    }
+}
