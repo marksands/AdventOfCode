baef3f9bcb0bae0f08b78a9d85d7a3608bcf7f42
Mark Sands <marksands07@gmail.com>
2018-12-31 15:25:08 -0600

    Day 10 initial

---
+++ AOC2015/AOC2015/Day10/Day10.swift
@@ -0,0 +1,14 @@
+import Foundation
+import AdventOfCode
+
+public final class Day10: Day {
+    private let input = "1321131112"
+    
+    public override func part1() -> String {
+        return "TBD"
+    }
+    
+    public override func part2() -> String {
+        return "TBD"
+    }
+}
---
+++ AOC2015/AOC2015/Day10/Day10Tests.swift
@@ -0,0 +1,14 @@
+import XCTest
+import AOC2015
+
+class AOC2015_Day10_Tests: XCTestCase {
+    let day = Day10()
+    
+    func testPart1() {
+        XCTAssertEqual("TBD", day.part1())
+    }
+    
+    func testPart2() {
+        XCTAssertEqual("TBD", day.part2())
+    }
+}
