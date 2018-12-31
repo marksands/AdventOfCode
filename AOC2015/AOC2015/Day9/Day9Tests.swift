42fac3257d534d6fec74227463f5d6b4229181d8
Mark Sands <marksands07@gmail.com>
2018-12-31 10:09:44 -0600

    Day 9 initial

---
+++ AOC2015/AOC2015/Day9/Day9Tests.swift
@@ -0,0 +1,14 @@
+import XCTest
+import AOC2015
+
+class AOC2015_Day9_Tests: XCTestCase {
+    let day = Day9()
+    
+    func testPart1() {
+        XCTAssertEqual("TBD", day.part1())
+    }
+    
+    func testPart2() {
+        XCTAssertEqual("TBD", day.part2())
+    }
+}
