b9e9806efbf5dc372e07cb55674a79ca3252c03f
Mark Sands <marksands07@gmail.com>
2019-01-01 21:54:38 -0600

    Improved EqualSubsequences

--- AOC2015/AOC2015/Day10/Day10.swift
+++ AOC2015/AOC2015/Day10/Day10.swift
@@ -2,7 +2,7 @@ import Foundation
 import AdventOfCode
 
 public final class Day10: Day {
-    private let input = "1321131112"
+    private let input = Input().trimmedRawInput()
     
     public override func part1() -> String {
         return String(lookAndSayLength(times: 40))
@@ -15,7 +15,9 @@ public final class Day10: Day {
     private func lookAndSayLength(times: Int) -> Int {
         var result = input
         (0..<times).forEach { _ in
-            result = result.equalSubsequences().reduce(into: "") { $0 += "\($1.count)\($1.first!)" }
+            result = result.equalSubsequences().reduce(into: "") {
+                $0 += "\($1.count)\($1.first!)"
+            }
         }
         return result.count
     }
--- AdventOfCode/AdventOfCode/EqualSubsequences.swift
+++ AdventOfCode/AdventOfCode/EqualSubsequences.swift
@@ -1,23 +1,10 @@
-extension Array where Element: Equatable {
+extension RangeReplaceableCollection where Element: Equatable {
     public func equalSubsequences() -> [SubSequence] {
         return reduce(into: []) { seed, result in
-            if seed.last?.last == result {
-                seed[seed.count-1] = seed.last! + [result]
-            } else {
-                seed.append([result])
+            if !(seed.last?.contains(result) ?? false) {
+                seed.append(SubSequence())
             }
-        }
-    }
-}
-
-extension String {
-    public func equalSubsequences() -> [Substring] {
-        return reduce(into: []) { seed, result in
-            if seed.last?.last == result {
-                seed[seed.count-1] = seed.last! + [result]
-            } else {
-                seed.append(Substring(String(result)))
-            }
+            seed[seed.count - 1].append(result)
         }
     }
 }
