208f1c020b246cb20154353be135f80377a0a920
Mark Sands <marksands07@gmail.com>
2019-01-09 12:55:21 -0600

    Adds Scan and Lazy Scan

---
+++ AdventOfCode/AdventOfCode/Scan.swift
@@ -0,0 +1,50 @@
+extension Sequence {
+    public func scan<ResultElement>(_ initial: ResultElement, _ nextPartialResult: (ResultElement, Element) -> ResultElement) -> [ResultElement] {
+        var result = [initial]
+        for x in self {
+            result.append(nextPartialResult(result.last!, x))
+        }
+        return result
+    }
+}
+
+public struct LazyScanIterator<Base : IteratorProtocol, ResultElement> : IteratorProtocol {
+    private var nextElement: ResultElement?
+    private var base: Base
+    private let nextPartialResult: (ResultElement, Base.Element) -> ResultElement
+
+    public init(nextElement: ResultElement?, base: Base, _ nextPartialResult: @escaping (ResultElement, Base.Element) -> ResultElement) {
+        self.nextElement = nextElement
+        self.base = base
+        self.nextPartialResult = nextPartialResult
+    }
+    
+    public mutating func next() -> ResultElement? {
+        return nextElement.map { result in
+            nextElement = base.next().map { nextPartialResult(result, $0) }
+            return result
+        }
+    }
+}
+
+public struct LazyScanSequence<Base: Sequence, ResultElement> : LazySequenceProtocol {
+    private let initial: ResultElement
+    private let base: Base
+    private let nextPartialResult: (ResultElement, Base.Element) -> ResultElement
+
+    public init(initial: ResultElement, base: Base, _ nextPartialResult: @escaping (ResultElement, Base.Element) -> ResultElement) {
+        self.initial = initial
+        self.base = base
+        self.nextPartialResult = nextPartialResult
+    }
+    
+    public func makeIterator() -> LazyScanIterator<Base.Iterator, ResultElement> {
+        return LazyScanIterator(nextElement: initial, base: base.makeIterator(), nextPartialResult)
+    }
+}
+
+extension LazySequenceProtocol {
+    public func scan<ResultElement>(_ initial: ResultElement, _ nextPartialResult: @escaping (ResultElement, Element) -> ResultElement) -> LazyScanSequence<Self, ResultElement> {
+        return LazyScanSequence(initial: initial, base: self, nextPartialResult)
+    }
+}
