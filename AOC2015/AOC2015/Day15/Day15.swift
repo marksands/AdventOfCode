9d95e66c342972c626b7bff6b5f048af05957766
Mark Sands <marksands07@gmail.com>
2019-07-21 23:27:22 -0500

    Day 15

--- AOC2015/AOC2015/Day15/Day15.swift
+++ AOC2015/AOC2015/Day15/Day15.swift
@@ -11,26 +11,59 @@ public struct Ingredient {
 }
 
 public final class Day15: Day {
+    private let ingredients: [Ingredient]
+    
     public init(input: [String] = Input().trimmedInputCharactersByNewlines()) {
-        super.init()
-
         let regex = Regex(pattern: "^(\\w+): capacity (-?\\d+), durability (-?\\d+), flavor (-?\\d+), texture (-?\\d+), calories (-?\\d+)$")
         
-        let ingredients = input.map { line -> Ingredient in
+        ingredients = input.map { line -> Ingredient in
             let matches = regex.matches(in: line)!
             return Ingredient(name: matches[1], capacity: Int(matches[2])!, durability: Int(matches[3])!, flavor: Int(matches[4])!, texture: Int(matches[5])!, calories: Int(matches[6])!)
         }
-
-        print(ingredients)
-    }
-    
-    public override func part1() -> String {
-        //[1, 2].partit
         
-        return ""
+        super.init()
     }
     
+    // TODO: optimize
+    public override func part1() -> String {
+        let scores = IntegerPartitionSum(sum: 100)
+            .lazy
+            .filter({ $0.count == self.ingredients.count })
+            .flatMap({ ingredientPartition -> [Int] in
+                return Array(ingredientPartition.permutations()).map { permuation -> Int in
+                    let formulas = zip(self.ingredients, permuation)
+                        .map { [$0.capacity * $1, $0.durability * $1, $0.flavor * $1, $0.texture * $1] }
+                    return
+                        max(0, formulas.reduce(0) { $0 + $1[0] }) *
+                            max(0, formulas.reduce(0) { $0 + $1[1] }) *
+                            max(0, formulas.reduce(0) { $0 + $1[2] }) *
+                            max(0, formulas.reduce(0) { $0 + $1[3] })
+                }
+            })
+        
+        return "\(scores.max()!)"
+    }
+    
+    // TODO: optimize
     public override func part2() -> String {
-        return ""
+        let scores = IntegerPartitionSum(sum: 100)
+            .lazy
+            .filter({ $0.count == self.ingredients.count })
+            .flatMap({ ingredientPartition -> [Int] in
+                return Array(ingredientPartition.permutations()).compactMap { permuation -> Int? in
+                    let formulas = zip(self.ingredients, permuation)
+                        .map { [$0.capacity * $1, $0.durability * $1, $0.flavor * $1, $0.texture * $1, $0.calories * $1] }
+                    
+                    guard formulas.reduce(0, { $0 + $1[4] }) == 500 else { return nil }
+                    
+                    return
+                        max(0, formulas.reduce(0) { $0 + $1[0] }) *
+                            max(0, formulas.reduce(0) { $0 + $1[1] }) *
+                            max(0, formulas.reduce(0) { $0 + $1[2] }) *
+                            max(0, formulas.reduce(0) { $0 + $1[3] })
+                }
+            })
+        
+        return "\(scores.max()!)"
     }
 }
--- AOC2015/AOC2015/Day15/Day15Tests.swift
+++ AOC2015/AOC2015/Day15/Day15Tests.swift
@@ -9,16 +9,16 @@ Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
     
     func testSample() {
         let day = Day15(input: sample.components(separatedBy: .newlines))
-        day.part1()
+        _ = day.part1()
     }
     
     func testPart1() {
         let day = Day15()
-        XCTAssertEqual("", day.part1())
+        XCTAssertEqual("21367368", day.part1())
     }
     
     func testPart2() {
         let day = Day15()
-        XCTAssertEqual("", day.part2())
+        XCTAssertEqual("1766400", day.part2())
     }
 }
