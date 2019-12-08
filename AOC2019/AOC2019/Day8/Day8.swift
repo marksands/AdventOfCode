6a24e40c84931b8d4de7d3cec0a3d8a78f033849
Mark Sands <marksands07@gmail.com>
2019-12-08 13:22:34 -0600

    2019 day 8, golfing

--- AOC2019/AOC2019/Day8/Day8.swift
+++ AOC2019/AOC2019/Day8/Day8.swift
@@ -2,41 +2,27 @@ import Foundation
 import AdventOfCode
 
 public final class Day8: Day {
-    private let input = Input().trimmedRawInput()
+    private let layers = Array(Input().trimmedRawInput()).chunks(ofSize: 25 * 6)
     
     public override func part1() -> String {
-        let fewestZeros = Array(input).chunks(ofSize: 25 * 6)
-            .min(by: { $0.count { $0 == "0" } < $1.count { $0 == "0" } })!
-        
+        let fewestZeros = layers.min(by: { $0.count { $0 == "0" } < $1.count { $0 == "0" } })!
         return String(fewestZeros.count { String($0) == "1" } * fewestZeros.count { String($0) == "2" })
     }
     
     public override func part2() -> String {
-        var picture = Array(repeating: "2", count: 25 * 6)
+        var picture = Array(repeating: " ", count: 25 * 6)
         
-        Array(input).chunks(ofSize: 25 * 6).forEach { layer in
-            picture = zip(picture, layer).map { p, l -> String in
-                p == "2" ? String(l) : p
+        layers.reversed().forEach { layer in
+            picture = zip(picture, layer).map { p, l in
+                return l == "0" ? "⬛️" : l == "1" ? "⬜️" : p
             }
         }
         
-        printPicture(picture)
-                
+        picture.chunks(ofSize: 25).forEach { row in
+            print(row.joined())
+        }
+
         return ""
-    }
-    
-    private func printPicture(_ picture: [String]) {
-        picture.chunks(ofSize: 25).forEach { y in
-            var result = ""
-            y.forEach { x in
-                if x == "1" {
-                    result += "⬜️"
-                } else {
-                    result += "⬛️"
-                }
-            }
-            print(result)
-        }
     }
 }
 
