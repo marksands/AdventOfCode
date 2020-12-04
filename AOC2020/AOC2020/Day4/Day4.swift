da3b1ab889cefc2191338ec9e09d0165e5981f45
Mark Sands <mark.sands@disneystreaming.com>
2020-12-03 22:31:33 -0600

    2020 Day 4 skeleton

---
+++ AOC2020/AOC2020/Day4/Day4.swift
@@ -0,0 +1,20 @@
+import Foundation
+import AdventOfCode
+
+public final class Day4: Day {
+	private let lines: [String]
+
+	public init(lines: [String] = Input().trimmedInputCharactersByNewlines()) {
+		self.lines = lines
+
+		super.init()
+	}
+
+	public override func part1() -> String {
+		return ""
+	}
+
+	public override func part2() -> String {
+		return ""
+	}
+}
