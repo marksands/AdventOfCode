55c70c4a8219cd9abcdd542ea15c80e38941c5ad
Mark Sands <mark.sands@disneystreaming.com>
2020-12-04 10:12:19 -0600

    2020 Day 4

--- AOC2020/AOC2020/Day4/Day4.swift
+++ AOC2020/AOC2020/Day4/Day4.swift
@@ -2,87 +2,39 @@ import Foundation
 import AdventOfCode
 
 enum Field: String, CaseIterable {
-	case byr
-	case iyr
-	case eyr
-	case hgt
-	case hcl
-	case ecl
-	case pid
-	case cid
+	case byr, iyr, eyr, hgt, hcl, ecl, pid, cid
+
+	static var requiredCases: [Field] {
+		allCases.dropLast()
+	}
 }
 
 public final class Day4: Day {
-
 	struct Passport {
 		let fields: [Field: String]
 
-		var valid: Bool {
-			return fields.keys.count == 8 ||
-			(isMissingCID && fields.keys.count == 7)
+		var isValid: Bool {
+			Field.requiredCases.allSatisfy(fields.keys.contains)
 		}
 
-		var isMissingCID: Bool {
-			return !fields.keys.contains(Field.cid)
-		}
-
-		var isVeryValid: Bool {
-			return isBirth && isIssue && isExpirationYear && isHeight && isHair && isEye && isPassport
-		}
-
-		var isBirth: Bool {
-			fields.keys.contains(Field.byr) &&
-				Int(fields[.byr]!)! >= 1920 &&
-				Int(fields[.byr]!)! <= 2002
-		}
-
-		var isIssue: Bool {
-			fields.keys.contains(Field.iyr) &&
-				Int(fields[.iyr]!)! >= 2010 &&
-				Int(fields[.iyr]!)! <= 2020
-		}
-
-		var isExpirationYear: Bool {
-			fields.keys.contains(Field.eyr) &&
-				Int(fields[.eyr]!)! >= 2020 &&
-				Int(fields[.eyr]!)! <= 2030
-		}
-
-		var isHeight: Bool {
-			guard fields.keys.contains(Field.hgt) else { return false }
-
-			let parser = Parser.int.take(Parser.remaining).map { (i: Int, s: Substring) -> Bool in
-				if s == "cm" {
-					return (i >= 150 && i <= 193)
-				} else if s == "in" {
-					return (i >= 59 && i <= 76)
-				} else {
-					return false
+		var hasValidData: Bool {
+			let rules = [birthRule, issueRule, expirationRule, heightRule, hairRule, eyeRule, passportRule]
+			return zip(Field.requiredCases, rules)
+				.allSatisfy { field, rule in
+					fields[field].flatMap { rule.run($0[...]).match } ?? false
 				}
-			}
-
-			return parser.run(fields[.hgt]![...]).match ?? false
 		}
 
-		var isHair: Bool {
-			guard fields.keys.contains(Field.hcl) else { return false }
-			let hcl = fields[Field.hcl]!
-			return hcl.hasPrefix("#") && hcl.dropFirst().count == 6
-			//return fields[Field.hcl]!.count == 6
-			//		hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-
-		}
-
-		var isEye: Bool {
-			guard fields.keys.contains(Field.ecl) else { return false }
-			let ch = fields[Field.ecl]!
-			return ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(ch)
-		}
-
-		var isPassport: Bool {
-			guard fields.keys.contains(Field.pid) else { return false }
-			return Int(fields[Field.pid, default: ""]) != nil && fields[Field.pid]!.count == 9
-		}
+		let birthRule = Parser.int.map { (1920...2002).contains($0) }
+		let issueRule = Parser.int.map { (2010...2020).contains($0) }
+		let expirationRule = Parser.int.map { (2020...2030).contains($0) }
+		let heightRule = Parser.oneOf(
+			Parser.int.map { (150...193).contains($0) }.skip(Parser.literal("cm")),
+			Parser.int.map { (59...76).contains($0) }.skip(Parser.literal("in"))
+		)
+		let hairRule = Parser.prefix("#").take(Parser.prefix(while: \.isHexDigit)).map { $0.count == 6 }
+		let eyeRule = Parser.oneOf(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].map(Parser.literal)).map { true }
+		let passportRule = Parser.prefix(while: \.isNumber).map { $0.count == 9 }
 	}
 
 	var passports: [Passport] = []
@@ -100,21 +52,20 @@ public final class Day4: Day {
 		let passports = Parser
 			.oneOf(anyField)
 			.zeroOrMore()
-			.map { fields in
-				Passport(fields: fields.reduce([Field: String]()) { $0.merging(with: $1) })
-			}
+			.map { $0.reduce([Field: String]()) { $0.merging(with: $1) } }
+			.map(Passport.init)
 			.zeroOrMore(separatedBy: Parser.whitespacesAndNewlines)
 
 		self.passports = passports.run(input[...]).match ?? []
 	}
 
 	public override func part1() -> String {
-		let valid = passports.count(where: { $0.valid })
-		return "\(valid)"
+		let count = passports.count(where: { $0.isValid })
+		return "\(count)"
 	}
 
 	public override func part2() -> String {
-		let valid = passports.count(where: { $0.isVeryValid })
-		return "\(valid)"
+		let count = passports.count(where: { $0.hasValidData })
+		return "\(count)"
 	}
 }
