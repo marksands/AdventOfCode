bfae8b931dfe083d50d016fe0c10db79e247adfc
Mark Sands <mark.sands@disneystreaming.com>
2020-12-05 00:29:01 -0600

    2020 Day 5

--- AOC2020/AOC2020/Day5/Day5.swift
+++ AOC2020/AOC2020/Day5/Day5.swift
@@ -2,21 +2,19 @@ import Foundation
 import AdventOfCode
 
 public final class Day5: Day {
-	var lines: [String] = []
+	var seatIds: [Int] = []
 
 	public init(input: [String] = Input().trimmedInputCharactersByNewlines()) {
-		self.lines = input
 		super.init()
+		self.seatIds = input.map(seatId)
 	}
 
 	public override func part1() -> String {
-		let id = lines.map { seatId($0) }.max(by: <)!
-		return String(id)
+		return String(seatIds.max(by: <)!)
 	}
 
 	public override func part2() -> String {
-		let allIds = lines.map { self.seatId($0) }
-		let seatId = allIds.first(where: { !allIds.contains($0+1) && allIds.contains($0+2) })! + 1
+		let seatId = seatIds.first(where: { !seatIds.contains($0+1) && seatIds.contains($0+2) })! + 1
 		return String(seatId)
 	}
 
