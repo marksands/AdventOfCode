14a9dbcdbefdebc42251f341a122b3a3e582428f
Mark Sands <mark.sands@disneystreaming.com>
2020-12-05 00:32:25 -0600

    🏌🏻‍♂️

--- AOC2020/AOC2020/Day5/Day5.swift
+++ AOC2020/AOC2020/Day5/Day5.swift
@@ -6,7 +6,9 @@ public final class Day5: Day {
 
 	public init(input: [String] = Input().trimmedInputCharactersByNewlines()) {
 		super.init()
-		self.seatIds = input.map(seatId)
+		self.seatIds = input.map {
+			Int($0.map { $0 == "B" ? "1" : $0 == "R" ? "1" : "0" }.joined(), radix: 2)!
+		}
 	}
 
 	public override func part1() -> String {
@@ -14,11 +16,6 @@ public final class Day5: Day {
 	}
 
 	public override func part2() -> String {
-		let seatId = seatIds.first(where: { !seatIds.contains($0+1) && seatIds.contains($0+2) })! + 1
-		return String(seatId)
-	}
-
-	func seatId(_ string: String) -> Int {
-		return Int(string.map { $0 == "B" ? "1" : $0 == "R" ? "1" : "0" }.joined(), radix: 2)!
+		return String(seatIds.first(where: { !seatIds.contains($0+1) && seatIds.contains($0+2) })! + 1)
 	}
 }
