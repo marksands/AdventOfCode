542255b2e49bf9e37e320583ba0eea345619a9c3
Mark Sands <mark.sands@disneystreaming.com>
2020-12-06 22:57:48 -0600

    2020 Day 7

---
+++ AOC2020/AOC2020/Day7/Day7.swift
@@ -0,0 +1,19 @@
+import Foundation
+import AdventOfCode
+
+public final class Day7: Day {
+	private var input: [String] = []
+
+	public init(input: String = Input().trimmedRawInput()) {
+		super.init()
+		self.input = input.components(separatedBy: "\n\n")
+	}
+
+	public override func part1() -> String {
+		return ""
+	}
+
+	public override func part2() -> String {
+		return ""
+	}
+}
