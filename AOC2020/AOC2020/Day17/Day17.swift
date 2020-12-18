dfc9beee34698f5f05ea69adc5ace9b3bc75973d
Mark Sands <mark.sands@disneystreaming.com>
2020-12-17 22:10:03 -0600

    2020 Day 17

--- AOC2020/AOC2020/Day17/Day17.swift
+++ AOC2020/AOC2020/Day17/Day17.swift
@@ -2,18 +2,89 @@ import Foundation
 import AdventOfCode
 
 public final class Day17: Day {
-	private var input: [String] = []
+	private var initialGrid: [Position: Bool] = [:]
 
-	public init(input: [String] = Input().trimmedInputCharactersByNewlines()) {
+	public init(input: String = Input().trimmedRawInput()) {
 		super.init()
-		self.input = input
+
+		for (y, line) in input.lines.enumerated() {
+			for (x, c) in line.enumerated() {
+				if c == "#" {
+					initialGrid[Position(x: x, y: y, z: 0, w: 0)] = true
+				}
+			}
+		}
 	}
 
 	public override func part1() -> String {
-		return ""
+		var grid = initialGrid
+
+		for _ in (0..<6) {
+			var mark: [Position: Int] = [:]
+
+			for (key, _) in grid {
+				for z in (-1...1) {
+					for y in (-1...1) {
+						for x in (-1...1) {
+							if z == y, y == x, x == 0 {
+								continue
+							}
+							mark[Position(x: key.x + x, y: key.y + y, z: key.z + z), default: 0] += 1
+						}
+					}
+				}
+			}
+
+			var sweep: [Position: Bool] = [:]
+
+			for (key, value) in mark {
+				if !grid[key, default: false], value == 3 {
+					sweep[key] = true
+				} else if grid[key, default: false] && (value == 2 || value == 3) {
+					sweep[key] = true
+				}
+			}
+
+			grid = sweep
+		}
+
+		return String(grid.values.count(where: { $0 }))
 	}
 
 	public override func part2() -> String {
-		return ""
+		var grid = initialGrid
+
+		for _ in (0..<6) {
+			var mark: [Position: Int] = [:]
+
+			for (key, _) in grid {
+				for w in (-1...1) {
+					for z in (-1...1) {
+						for y in (-1...1) {
+							for x in (-1...1) {
+								if w == z, z == y, y == x, x == 0 {
+									continue
+								}
+								mark[Position(x: key.x + x, y: key.y + y, z: key.z + z, w: key.w + w), default: 0] += 1
+							}
+						}
+					}
+				}
+			}
+
+			var sweep: [Position: Bool] = [:]
+
+			for (key, value) in mark {
+				if !grid[key, default: false], value == 3 {
+					sweep[key] = true
+				} else if grid[key, default: false] && (value == 2 || value == 3) {
+					sweep[key] = true
+				}
+			}
+
+			grid = sweep
+		}
+
+		return String(grid.values.count(where: { $0 }))
 	}
 }
--- AOC2020/AOC2020/Day17/Day17Tests.swift
+++ AOC2020/AOC2020/Day17/Day17Tests.swift
@@ -5,10 +5,10 @@ class AOC2020_Day17_Tests: XCTestCase {
 	let day = Day17()
 
 	func testPart1() {
-		XCTAssertEqual("", day.part1())
+		XCTAssertEqual("209", day.part1())
 	}
 
 	func testPart2() {
-		XCTAssertEqual("", day.part2())
+		XCTAssertEqual("1492", day.part2())
 	}
 }
