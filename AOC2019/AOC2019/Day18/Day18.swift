1de6c234a4573897f5ce3f2a282b7981c27ec7aa
Mark Sands <mark.sands@disneystreaming.com>
2020-12-29 13:53:57 -0600

    2019 day 18 cleanup

--- AOC2019/AOC2019/Day18/Day18.swift
+++ AOC2019/AOC2019/Day18/Day18.swift
@@ -46,19 +46,12 @@ public final class Day18: Day {
 			}
 		}
 
-//		let short = shortestPath(from: Path(path: [player], keyring: [], player: player), to: keyPositions["g"]!)!
-//		print(short)
-//		print(short.path.count)
-//		return ""
 
 		let result = pathfind(from: player)
 		print("---------- PATH ---------- ")
 		print(result)
 
 		let stepsRequired = result.steps
-
-		// TODO: (Array+append is too slow. Whole thing runs in about 75 minutes)
-//		printPath(result)
 
 		return "\(stepsRequired)"
     }
@@ -73,59 +66,18 @@ public final class Day18: Day {
 		}
 	}
 
-//	private func printPath(_ path: Path) {
-//		let allKeys = path.keyring.map { $0 }
-//			.filter { $0 != "." }
-//			.filter { String($0) == String($0).lowercased() }
-//
-//		let allDoors = path.keyring.map { $0 }
-//			.filter { $0 != "." }
-//			.filter { String($0) == String($0).uppercased() }
-//
-//		for (y, row) in grid.enumerated() {
-//			var resultLine = ""
-//			for (x, tile) in row.enumerated() {
-//				let p = Position(x: x, y: y)
-//
-//				if allKeys.contains(tile) {
-//					resultLine += "K"
-//				} else if allDoors.contains(tile) {
-//					resultLine += "D"
-//				} else if path.path.contains(p) {
-//					resultLine += "x"
-//				} else if tile == "." {
-//					resultLine += " "
-//				} else {
-//					resultLine += String(tile)
-//				}
-//			}
-//			print(resultLine)
-//		}
-//	}
-
 	private func pathfind(from player: Position) -> Path {
 		var possiblePaths = LinkedList<Path>([Path(keyring: [], player: player, steps: 0)])
-
-		// NOTE: It performed 30 200 000 iterations
 
 		let allkeys = keyPositions.keys.sorted()
 
 		var c = 0
 
 		while let path = possiblePaths.popFirst() {
-//			if ["a"].isContainedWithin(path.keyring) {
 			if isContainedWithin(allkeys, path.keyring) {
 				return path
 			} else {
-//				for possiblePath in self.possiblePaths(from: path) {
-//					c += 1
-//
-//					possiblePaths.append(possiblePath)
-//
-//					if c % 10_000 == 0 {
-//						print("step \(c)...")
-//					}
-//				}
+
 				for (step, tile) in possibleSteps(from: path) {
 					var newKeyring = path.keyring
 					newKeyring.insert(tile)
@@ -148,46 +100,6 @@ public final class Day18: Day {
 		fatalError("You're algorithm is bad.")
 	}
 
-//	private func possiblePaths(from path: Path) -> [Path] {
-//		let allReachableKeysFromStartingPosition = keyPositions.lazy
-//			.filter { kv in !path.keyring.contains(kv.key) }
-//			.compactMap { self.shortestPath(from: path, to: $0.value) }
-//
-//		return Array(allReachableKeysFromStartingPosition)
-//	}
-//
-//	var c = 0
-
-//	private func shortestPath(from path: Path, to key: Position) -> Path? {
-//		var possiblePaths = [path]
-//
-//		if let path = pathCache[Double(p: path.player, p2: key)] { return path }
-//
-//		while let path = possiblePaths.popFirst() {
-//			if path.player == key {
-//				// `player: key` -> new starting position is where the last key was found.
-//				print("Found shortest path to \(keysAtPosition[key]!)")
-//				return path
-//			} else {
-//				for (step, tile) in possibleSteps(from: path) {
-//					var newKeyring = path.keyring
-//					newKeyring.insert(tile)
-//					let possiblePath = Path(keyring: newKeyring, player: step, steps: path.steps + 1)
-//
-//					if seen.insert(PathCache(player: step, keys: newKeyring)).inserted {
-//						possiblePaths.append(possiblePath)
-//						c += 1
-//						if c % 100_000 == 0 {
-//							print("step \(c)...")
-//						}
-//					}
-//				}
-//			}
-//		}
-//
-//		return nil
-//	}
-//
 	private func possibleSteps(from path: Path) -> [(Position, Character)] {
 		let steps: [(Position, Character)] = [path.player.north(), path.player.south(), path.player.west(), path.player.east()]
 			.lazy
@@ -204,7 +116,6 @@ public final class Day18: Day {
 	@inline(__always)
 	private func hasKeyForDoor(_ keyring: Set<Character>, door: Character) -> Bool {
 		return isContainedWithin([Character(door.lowercased())], keyring) && door.isUppercase
-		//return keyring.contains(Character(door.lowercased())) && uppercaseLetters.contains(door)
 	}
 
 	private func isContainedWithin(_ sortedSelf: [Character], _ other: Set<Character>) -> Bool {
