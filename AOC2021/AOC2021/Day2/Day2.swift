5132a10430870a609bd38f3f8af959b8a6e6911c
Mark Sands <marksands07@gmail.com>
2021-12-01 23:40:20 -0600

    2021 day 2

--- AOC2021/AOC2021/Day2/Day2.swift
+++ AOC2021/AOC2021/Day2/Day2.swift
@@ -2,11 +2,50 @@ import Foundation
 import AdventOfCode
 
 public final class Day2: Day {
-	public override func part1() -> String {
-        fatalError()
+	private let lines: [String]
+
+	public init(lines: [String] = Input().trimmedInputCharactersByNewlines()) {
+		self.lines = lines
+		super.init()
 	}
 
+	public override func part1() -> String {
+		var horizontal = 0
+		var depth = 0
+		
+		for line in lines {
+			let value = line.ints[0]
+			
+			if line.starts(with: "forward") {
+				horizontal += value
+			} else if line.starts(with: "down") {
+				depth += value
+			} else {
+				depth -= value
+			}
+		}
+		
+		return String(horizontal * depth)
+	}
+	
 	public override func part2() -> String {
-        fatalError()
+		var aim = 0
+		var horizontal = 0
+		var depth = 0
+		
+		for line in lines {
+			let value = line.ints[0]
+
+			if line.starts(with: "forward") {
+				horizontal += value
+				depth += (aim * value)
+			} else if line.starts(with: "down") {
+				aim += value
+			} else {
+				aim -= value
+			}
+		}
+		
+		return String(horizontal * depth)
     }
 }
--- AOC2021/AOC2021/Day2/Day2Tests.swift
+++ AOC2021/AOC2021/Day2/Day2Tests.swift
@@ -5,10 +5,10 @@ class AOC2021_Day2_Tests: XCTestCase {
 	let day = Day2()
 
     func testPart1() {
-        XCTAssertEqual("", day.part1())
+        XCTAssertEqual("1488669", day.part1())
     }
 
     func testPart2() {
-        XCTAssertEqual("", day.part2())
+        XCTAssertEqual("1176514794", day.part2())
     }
 }
