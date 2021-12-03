7221db4db92eab94754eef0e60f955dff3801e4f
Mark Sands <marksands07@gmail.com>
2021-12-03 01:55:36 -0600

    2021 day 3.1

--- AOC2021/AOC2021/Day3/Day3.swift
+++ AOC2021/AOC2021/Day3/Day3.swift
@@ -1,12 +1,55 @@
 import Foundation
 import AdventOfCode
 
+extension Collection where Element: Hashable {
+	func mostCommonElement() -> Element {
+		var dictionary: [Element: Int] = [:]
+		for value in self {
+			dictionary[value, default: 0] += 1
+		}
+		return dictionary.max { $0.value < $1.value }!.key
+	}
+
+	func leastCommonElement() -> Element {
+		var dictionary: [Element: Int] = [:]
+		for value in self {
+			dictionary[value, default: 0] += 1
+		}
+		return dictionary.min { $0.value < $1.value }!.key
+	}
+}
+
+
 public final class Day3: Day {
+	private let lines: [String]
+
+	public init(lines: [String] = Input().trimmedInputCharactersByNewlines()) {
+		self.lines = lines
+		super.init()
+	}
+
     public override func part1() -> String {
-        fatalError()
+		let width = 12 // lines[0].count maybe?
+
+		var bits: [[String]] = Array(repeating: [String](), count: width)
+
+		for line in lines {
+			let nums = line.exploded()
+			for i in (0..<width) {
+				bits[i].append(nums[i])
+			}
+		}
+		
+		let gammaBinary = bits.map { $0.mostCommonElement() }.joined()
+		let epsilonBinary = bits.map { $0.leastCommonElement() }.joined()
+
+		let gamma = Int(gammaBinary, radix: 2)!
+		let epsilon = Int(epsilonBinary, radix: 2)!
+
+		return String(gamma * epsilon)
     }
 
     public override func part2() -> String {
-        fatalError()
+		return ""
     }
 }
--- AOC2021/AOC2021/Day3/Day3Tests.swift
+++ AOC2021/AOC2021/Day3/Day3Tests.swift
@@ -5,10 +5,11 @@ class AOC2021_Day3_Tests: XCTestCase {
 	let day = Day3()
 
 	func testPart1() {
-		XCTAssertEqual("", day.part1())
+		XCTAssertEqual("4001724", day.part1())
 	}
 
 	func testPart2() {
 		XCTAssertEqual("", day.part2())
 	}
 }
+ 
