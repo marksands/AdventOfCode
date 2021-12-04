56ed045b306172df86abe1a173f588ef21a9624e
Mark Sands <marksands07@gmail.com>
2021-12-04 16:05:00 -0600

    2021 day 4

--- AOC2021/AOC2021/Day4/Day4.swift
+++ AOC2021/AOC2021/Day4/Day4.swift
@@ -2,11 +2,85 @@ import Foundation
 import AdventOfCode
 
 public final class Day4: Day {
-    public override func part1() -> String {
-        fatalError()
-    }
+	typealias Board = [[Int]]
+	var boards: [Board] = []
+	var orderOfDrawnNumbers: [Int] = []
+	
+	let lines: [String]
+	
+	public init(lines: [String] = Input().trimmedInputCharactersByNewlines()) {
+		self.lines = lines
+		super.init()
+		parseInput()
+	}
+	
+	public override func part1() -> String {
+		var rollCall: [Int] = []
+		for drawn in orderOfDrawnNumbers {
+			rollCall.append(drawn)
+			for board in boards {
+				if boardHasBingo(board, drawn: rollCall) {
+					return String(score(of: board, drawn: rollCall))
+				}
+			}
+		}
+
+		return ":("
+	}
 
     public override func part2() -> String {
-        fatalError()
+		let totalBoards = boards.count
+		var wonBoardIndices: [Int] = []
+		
+		var rollCall: [Int] = []
+		for drawn in orderOfDrawnNumbers {
+			rollCall.append(drawn)
+			for (idx, board) in boards.enumerated() {
+				if boardHasBingo(board, drawn: rollCall), !wonBoardIndices.contains(idx) {
+					wonBoardIndices.append(idx)
+					if wonBoardIndices.count == totalBoards {
+						return String(score(of: board, drawn: rollCall))
+					}
+				}
+			}
+		}
+
+		return ":("
     }
+	
+	private func parseInput() {
+		var lines = self.lines
+		let drawnOrderLine = lines.removeFirst()
+		orderOfDrawnNumbers = drawnOrderLine.ints
+		
+		var currentBoard: Board = []
+		for line in lines.dropFirst() {
+			if line == "" {
+				boards.append(currentBoard)
+				currentBoard = []
+			} else {
+				currentBoard.append(line.ints)
+			}
+		}
+		boards.append(currentBoard)
+	}
+	
+	private func boardHasBingo(_ board: Board, drawn: [Int]) -> Bool {
+		for row in board {
+			if row.isContainedWithin(drawn) { return true }
+		}
+		
+		for (y, col) in board.enumerated() {
+			let column = col.enumerated().map { board[$0.offset][y] }
+			if column.isContainedWithin(drawn) { return true }
+		}
+		
+		return false
+	}
+	
+	private func score(of board: Board, drawn: [Int]) -> Int {
+		let allNumbers = board.flatMap { $0 }
+		let unmarkedNumbers = allNumbers.filter { !drawn.contains($0) }
+		return unmarkedNumbers.sum() * drawn.last!
+	}
 }
--- AOC2021/AOC2021/Day4/Day4Tests.swift
+++ AOC2021/AOC2021/Day4/Day4Tests.swift
@@ -5,10 +5,11 @@ class AOC2021_Day4_Tests: XCTestCase {
 	let day = Day4()
 
 	func testPart1() {
-		XCTAssertEqual("", day.part1())
+		XCTAssertEqual("55770", day.part1())
 	}
 
 	func testPart2() {
-		XCTAssertEqual("", day.part2())
+		XCTAssertEqual("2980", day.part2())
 	}
 }
+	
