e0127dc920dd03222100b8ae57b790b59719a1d4
Mark Sands <marksands07@gmail.com>
2021-12-06 22:21:05 -0600

    2021 day 6

--- AOC2021/AOC2021/Day6/Day6.swift
+++ AOC2021/AOC2021/Day6/Day6.swift
@@ -2,11 +2,34 @@ import Foundation
 import AdventOfCode
 
 public final class Day6: Day {
+	let initialState: [Int]
+	
+	public init(line: String = Input().trimmedRawInput()) {
+		self.initialState = line.ints
+	}
+	
     public override func part1() -> String {
-        fatalError()
-    }
+		return step(iterations: 80).string
+	}
 
     public override func part2() -> String {
-        fatalError()
+		return step(iterations: 256).string
+	}
+	
+	private func step(iterations: Int) -> Int {
+		var dictionary: [Int: Int] = initialState.countElements()
+		(0..<iterations).forEach { iteration in
+			var copy: [Int: Int] = [:]
+			for (key, value) in dictionary {
+				if key == 0 {
+					copy[6, default: 0] += value
+					copy[8, default: 0] += value
+				} else {
+					copy[key - 1, default: 0] += value
+				}
+			}
+			dictionary = copy
+		}
+		return dictionary.values.sum()
     }
 }
--- AOC2021/AOC2021/Day6/Day6Tests.swift
+++ AOC2021/AOC2021/Day6/Day6Tests.swift
@@ -5,10 +5,10 @@ class AOC2021_Day6_Tests: XCTestCase {
 	let day = Day6()
 
 	func testPart1() {
-		XCTAssertEqual("", day.part1())
+		XCTAssertEqual("380758", day.part1())
 	}
 
 	func testPart2() {
-		XCTAssertEqual("", day.part2())
+		XCTAssertEqual("1710623015163", day.part2())
 	}
 }
