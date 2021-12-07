911f1086bfe9a984196b1ad6e83ccd1117dfbf28
Mark Sands <marksands07@gmail.com>
2021-12-06 23:34:56 -0600

    2021 day 7

--- AOC2021/AOC2021/Day7/Day7.swift
+++ AOC2021/AOC2021/Day7/Day7.swift
@@ -2,11 +2,40 @@ import Foundation
 import AdventOfCode
 
 public final class Day7: Day {
-    public override func part1() -> String {
-        fatalError()
-    }
-
-    public override func part2() -> String {
-        fatalError()
-    }
+	let ints: [Int]
+	
+	public init(lines: String = Input().trimmedRawInput()) {
+		self.ints = lines.ints
+	}
+	
+	public override func part1() -> String {
+		var min = Int.max
+		for value in ints {
+			var sum = 0
+			for value2 in ints {
+				sum += abs(value2 - value)
+			}
+			min = Swift.min(sum, min)
+		}
+		return min.string
+	}
+	
+	public override func part2() -> String {
+		let maxNumber = ints.max()!
+		var min = Int.max
+		for value in (1..<maxNumber) {
+			var sum = 0
+			for value2 in ints {
+				let distance = triangluarNumber(abs(value - value2))
+				sum += distance
+			}
+			min = Swift.min(sum, min)
+		}
+		
+		return min.string
+	}
+	
+	func triangluarNumber(_ v: Int) -> Int {
+		return (v * (v + 1))/2
+	}
 }
--- AOC2021/AOC2021/Day7/Day7Tests.swift
+++ AOC2021/AOC2021/Day7/Day7Tests.swift
@@ -5,10 +5,12 @@ class AOC2021_Day7_Tests: XCTestCase {
 	let day = Day7()
 
 	func testPart1() {
-		XCTAssertEqual("", day.part1())
+		XCTAssertEqual("343441", day.part1())
 	}
 
+	// 842941 is not right
+	// 98928141 is not right
 	func testPart2() {
-		XCTAssertEqual("", day.part2())
+		XCTAssertEqual("98925151", day.part2())
 	}
 }
