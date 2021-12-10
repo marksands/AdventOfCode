95df3202dda26b1969c3d3f0f743b7f18e8d72de
Mark Sands <marksands07@gmail.com>
2021-12-10 13:59:02 -0600

    2021 Day 10

--- AOC2021/AOC2021/Day10/Day10.swift
+++ AOC2021/AOC2021/Day10/Day10.swift
@@ -2,11 +2,59 @@ import Foundation
 import AdventOfCode
 
 public final class Day10: Day {
+	let lines: [String]
+	
+	public init(lines: [String] = Input().trimmedInputCharactersByNewlines()) {
+		self.lines = lines
+	}
+	
     public override func part1() -> String {
-        fatalError()
-    }
+		return calculateScores().part1
+	}
 
     public override func part2() -> String {
-        fatalError()
+		return calculateScores().part2
+	}
+	
+	private func calculateScores() -> (part1: String, part2: String) {
+		let characters_open = ["(", "[", "{", "<"]
+		let characters_closing: [String: String] = ["(": ")", "[": "]", "{": "}", "<": ">"]
+		let character_closing_scores = [")": 3, "]": 57, "}": 1197, ">": 25137]
+		let character_closing_scores_incomplete = [")": 1, "]": 2, "}": 3, ">": 4]
+		
+		var incompleteScores: [Int] = []
+		var corruptedScore = 0
+		for line in lines {
+			var wasCorrupted = false
+			var stack: [String] = []
+			for character in line.exploded() {
+				if characters_open.contains(character) {
+					stack.append(character)
+				} else if !stack.isEmpty {
+					let top = stack.last!
+					if characters_closing[top] == character {
+						stack.removeLast()
+					} else {
+						// Encountered a corrupted line
+						corruptedScore += character_closing_scores[character]!
+						wasCorrupted = true
+						break
+					}
+				}
+			}
+			if !stack.isEmpty, !wasCorrupted {
+				// Encountered an incomplete line
+				var score = 0
+				while !stack.isEmpty {
+					let top = stack.removeLast()
+					let characterScore = character_closing_scores_incomplete[characters_closing[top]!]!
+					score = (score * 5) + characterScore
+				}
+				incompleteScores.append(score)
+			}
+		}
+		
+		let sorted = incompleteScores.sorted()
+		return (corruptedScore.string, sorted[sorted.count / 2].string)
     }
 }
--- AOC2021/AOC2021/Day10/Day10Tests.swift
+++ AOC2021/AOC2021/Day10/Day10Tests.swift
@@ -5,10 +5,10 @@ class AOC2021_Day10_Tests: XCTestCase {
 	let day = Day10()
 
 	func testPart1() {
-		XCTAssertEqual("", day.part1())
+		XCTAssertEqual("415953", day.part1())
 	}
 
 	func testPart2() {
-		XCTAssertEqual("", day.part2())
+		XCTAssertEqual("2292863731", day.part2())
 	}
 }
