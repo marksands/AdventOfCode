0b82eed3f531dd02d2848aaa59acb27bd6da51a0
Mark Sands <marksands07@gmail.com>
2021-12-17 13:19:19 -0600

    Fix fold bug with day 13

--- AOC2021/AOC2021/Day13/Day13.swift
+++ AOC2021/AOC2021/Day13/Day13.swift
@@ -5,19 +5,17 @@ public final class Day13: Day {
 	var input: String
 	var positions: [Position: String] = [:]
 	var foldDirections: [Position] = []
-	var maxWidth: Int
-	var maxHeight: Int
 
 	public init(input: String = Input().trimmedRawInput()) {
 		self.input = input
 				
-		let inputComponents = input.components(separatedBy: "\n\n")
-		for line in inputComponents[0].components(separatedBy: "\n") {
+		let inputComponents = input.groups
+		for line in inputComponents[0].lines {
 			let ints = String(line).ints
 			positions[Position(x: ints[0], y: ints[1])] = "#"
 		}
 				
-		for fold in inputComponents[1].components(separatedBy: "\n") {
+		for fold in inputComponents[1].lines {
 			let line = String(fold).replacingOccurrences(of: "fold along ", with: "")
 			if line.hasPrefix("x") {
 				foldDirections.append(Position(x: line.int, y: 0))
@@ -26,16 +24,11 @@ public final class Day13: Day {
 				foldDirections.append(Position(x: 0, y: line.int))
 			}
 		}
-		
-		maxWidth = positions.keys.map { $0.x }.max()!
-		maxHeight = positions.keys.map { $0.y }.max()!
 	}
 
     public override func part1() -> String {
 		let firstFold = foldDirections[0]
 		fold(firstFold)
-		
-		print("")
 		return printGrid().string
 	}
 	
@@ -43,37 +36,34 @@ public final class Day13: Day {
 		for foldDirection in foldDirections {
 			fold(foldDirection)
 		}
-		
 		return printGrid().string
 	}
 	
 	private func fold(_ foldDirection: Position) {
 		if foldDirection.x == 0 {
 			// fold-y
-			let foldedPositions: [Position] = Array(positions.keys.filter { $0.y >= foldDirection.y })
+			let foldedPositions: [Position] = Array(positions.keys.filter { $0.y > foldDirection.y })
 			
 			for position in foldedPositions {
 				positions[position] = nil
-				positions[Position(x: position.x, y: maxHeight - position.y)] = "#"
+				positions[Position(x: position.x, y: position.y - 2 * (position.y - foldDirection.y))] = "#"
 			}
-			
-			maxHeight = foldDirection.y - 1
 		} else {
 			// fold-x
-			let foldedPositions: [Position] = Array(positions.keys.filter { $0.x >= foldDirection.x })
+			let foldedPositions: [Position] = Array(positions.keys.filter { $0.x > foldDirection.x })
 			
 			for position in foldedPositions {
 				positions[position] = nil
-				positions[Position(x: maxWidth - position.x, y: position.y)] = "#"
+				positions[Position(x: position.x - 2 * (position.x - foldDirection.x), y: position.y)] = "#"
 			}
-			
-			maxWidth = foldDirection.x - 1
 		}
 	}
 	
 	@discardableResult
 	func printGrid() -> Int {
 		var dotCount = 0
+		let maxHeight = positions.filter { $0.value == "#" }.max(by: { $0.key.y < $1.key.y })!.key.y
+		let maxWidth = positions.filter { $0.value == "#" }.max(by: { $0.key.x < $1.key.x })!.key.x
 		
 		for y in (0...maxHeight) {
 			var string = ""
