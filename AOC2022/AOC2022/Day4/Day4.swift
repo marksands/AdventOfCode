3e49ee5b00e855abe15b74cd3e6ed37a3381847f
Mark Sands <marksands07@gmail.com>
2022-12-05 10:06:07 -0600

    2022 day 4

--- AOC2022/AOC2022/Day4/Day4.swift
+++ AOC2022/AOC2022/Day4/Day4.swift
@@ -10,10 +10,28 @@ public final class Day4: Day {
     }
 
     public override func part1() -> String {
-        return ""
+        var count = 0
+        for line in lines {
+            let nums = line.ints
+            let section1 = ClosedRange(uncheckedBounds: (nums[0], nums[1]))
+            let section2 = ClosedRange(uncheckedBounds: (nums[2], nums[3]))
+            if section1.isContainedWithin(section2) || section2.isContainedWithin(section1) {
+                count += 1
+            }
+        }
+        return count.string
     }
 
     public override func part2() -> String {
-        return ""
+        var count = 0
+        for line in lines {
+            let nums = line.ints
+            let section1 = ClosedRange(uncheckedBounds: (nums[0], nums[1]))
+            let section2 = ClosedRange(uncheckedBounds: (nums[2], nums[3]))
+            if section1.overlaps(section2) || section2.overlaps(section1) {
+                count += 1
+            }
+        }
+        return count.string
     }
 }
--- AOC2022/AOC2022/Day4/Day4Tests.swift
+++ AOC2022/AOC2022/Day4/Day4Tests.swift
@@ -5,11 +5,11 @@ class AOC2022_Day4_Tests: XCTestCase {
 	let day = Day4()
 
 	func testPart1() {
-		XCTAssertEqual("", day.part1())
+		XCTAssertEqual("513", day.part1())
 	}
 
 	func testPart2() {
-		XCTAssertEqual("", day.part2())
+		XCTAssertEqual("878", day.part2())
 	}
 }
 	
