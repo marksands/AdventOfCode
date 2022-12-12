b6b06949cfa4988de6a01716cfa765a322a2bcc9
Mark Sands <marksands07@gmail.com>
2022-12-12 07:40:21 -0600

    2022 day 10 cleanup

--- AOC2022/AOC2022/Day10/Day10.swift
+++ AOC2022/AOC2022/Day10/Day10.swift
@@ -35,65 +35,25 @@ public final class Day10: Day {
     }
 
     public override func part1() -> String {
-        // key: cycle, value: cycle * X
-        var sigs: [Int: Int] = [:]
-        var x = 1
-        var cycle = 0
-
-        var instructionQueue: [Int] = []
-
-        for command in commands {
-            if command == .noop {
-                cycle += 1
-                if let value = instructionQueue.first {
-                    x = x + value
-                    instructionQueue.removeFirst()
-                }
-                sigs[cycle] = cycle * x
-            } else {
-                instructionQueue.append(0)
-                instructionQueue.append(command.value)
-
-                cycle += 1
-                sigs[cycle] = cycle * x
-                if let value = instructionQueue.first {
-                    x = x + value
-                    instructionQueue.removeFirst()
-                }
-
-                cycle += 1
-                sigs[cycle] = cycle * x
-                if let value = instructionQueue.first {
-                    x = x + value
-                    instructionQueue.removeFirst()
-                }
-            }
-        }
-
-        let sum = sigs[20]! + sigs[60]! + sigs[100]! + sigs[140]! + sigs[180]! + sigs[220]!
-        return sum.string
+        return run().part1
     }
 
     public override func part2() -> String {
+        return run().part2
+    }
+
+    private func run() -> (part1: String, part2: String) {
         var x = 1
         var cycle = 0
         var instructionQueue: [Int] = []
-
+        var sigs: [Int: Int] = [:]
         var crt: [String] = []
-
-        // 40 wide by 6 high
-        // sprite starts left at 1? (X controls middle)
 
         for command in commands {
             if command == .noop {
                 cycle += 1
-                // draw
-                let horizontalX = (cycle-1) % 40
-                if x-1 == horizontalX || x == horizontalX || x+1 == horizontalX {
-                    crt.append("#")
-                } else {
-                    crt.append(".")
-                }
+                crt.append([x-1, x, x+1].contains(((cycle-1) % 40)) ? "#" : ".")
+                sigs[cycle] = cycle * x
 
                 if let value = instructionQueue.first {
                     x = x + value
@@ -104,13 +64,8 @@ public final class Day10: Day {
                 instructionQueue.append(command.value)
 
                 cycle += 1
-                // draw
-                var horizontalX = (cycle-1) % 40
-                if x-1 == horizontalX || x == horizontalX || x+1 == horizontalX {
-                    crt.append("#")
-                } else {
-                    crt.append(".")
-                }
+                crt.append([x-1, x, x+1].contains(((cycle-1) % 40)) ? "#" : ".")
+                sigs[cycle] = cycle * x
 
                 if let value = instructionQueue.first {
                     x = x + value
@@ -118,13 +73,8 @@ public final class Day10: Day {
                 }
 
                 cycle += 1
-                // draw
-                horizontalX = (cycle-1) % 40
-                if x-1 == horizontalX || x == horizontalX || x+1 == horizontalX {
-                    crt.append("#")
-                } else {
-                    crt.append(".")
-                }
+                crt.append([x-1, x, x+1].contains(((cycle-1) % 40)) ? "#" : ".")
+                sigs[cycle] = cycle * x
 
                 if let value = instructionQueue.first {
                     x = x + value
@@ -143,6 +93,7 @@ public final class Day10: Day {
         }
         print(result)
 
-        return "EZFPRAKL"
+        let sum = sigs[20]! + sigs[60]! + sigs[100]! + sigs[140]! + sigs[180]! + sigs[220]!
+        return (sum.string, "EZFPRAKL")
     }
 }
